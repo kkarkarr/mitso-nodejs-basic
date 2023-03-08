27b3003cddee7bcd3711971acb221c7855b4fab2
kkarkarr <lasvegas2992@gmail.com>
2023-03-08 23:01:24 +0300

    feat: rename done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,20 @@
+import fs from "fs/promises";
+import url from "url";
+
 const rename = async () => {
-    // Write your code here 
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    await fs.rename("src/fs/files/wrongFilename.txt", "src/fs/files/wrongFilename.md", (err) =>
+    {
+    if (err)
+        {
+            throw new Error ("FS operation failed");
+        }
+        else
+        {
+            console.log("File was renamed");
+        }
+    }
+    );
 };
 
 await rename();
