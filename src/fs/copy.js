ca74a754ece73c3475f34ef705c97607e3c4bbc4
kkarkarr <lasvegas2992@gmail.com>
2023-03-09 10:18:28 +0300

    feat: copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,23 @@
+import fs from "fs/promises";
+import url from "url";
+
+
+
 const copy = async () => {
-    // Write your code here 
-};
+    let dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    await fs.copyFile("src/fs/files", "src/fs/files_copy", (err) =>
+    {
+        if (err)
+        {
+            throw new Error ("FS operation failed");
+        }
+        else
+        {
+            console.log("Files were copied");
+        }
+    }
+    );
+}
+
 
 await copy();
