972f7875b4d14a2a5c4a9b24ad3e9c6aec949eb4
kkarkarr <lasvegas2992@gmail.com>
2023-03-13 16:12:34 +0300

    feat: made compress and decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createReadStream} from "fs";
+import { createWriteStream} from "fs";
+import path from "path"
+import { pipeline } from "stream";
+import * as url from "url"
+import { createGzip} from "zlib";
+
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const filesPath = path.join(_dirname, "files", "fileToCompress.txt")
+
 const compress = async () => {
-    // Write your code here 
+    const read = createReadStream(filesPath,"utf-8")
+    const arc = createGzip()
+    const write = createWriteStream(path.join(_dirname, "archive.gz"));
+
+    
+    try {
+        read.pipe(arc).pipe(write)
+        console.log("Archieve was made")
+    } catch (err) {
+        console.log(err.message)
+    }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { createReadStream} from "fs";
+import { createWriteStream} from "fs";
+import path from "path"
+import * as url from "url"
+import { createUnzip} from "zlib";
+
+const _dirname = url.fileURLToPath(new URL(".", import.meta.url))
+const filesPath = path.join(_dirname, "archive.gz")
+
 const decompress = async () => {
-    // Write your code here 
+    const read = createReadStream(filesPath)
+    const arc = createUnzip()
+    const write = createWriteStream(path.join(_dirname,"fileToCompress.txt"), {
+        encoding: "utf-8"
+    })
+
+    try {
+        read.pipe(arc).pipe(write)
+        console.log("Archieve was dearchieved")
+    } catch (err) {
+        console.log(err.message)
+    }
 };
 
 await decompress();
