bbc8e3ee3748122cddb07d154328c5e8784732e3
kkarkarr <lasvegas2992@gmail.com>
2023-03-13 15:28:08 +0300

    feat: add read to streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "fs"
+import {stdout} from "process"
+
 const read = async () => {
-    // Write your code here 
+    const streamIn = fs.createReadStream("src/streams/files/fileToRead.txt", "utf-8");
+    let data = "";
+    streamIn.on("data", chunk => data = data + chunk);
+    process.on("exit", code =>{
+        if(code === 0){
+            stdout.write(data);
+        }
+        else{
+            throw new Error ("Error!")
+        }
+    })
 };
 
 await read();
